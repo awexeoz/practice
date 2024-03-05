fef9ad622f9c9e90ced6bb955c5686e06abcad33
awexeoz <120705475+awexeoz@users.noreply.github.com>
2024-03-05 17:50:33 +0500

    Update task3.py

--- skcu/task3.py
+++ skcu/task3.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(content_types = ["text"])
 def repeat_message(message):
-    bot.send_message(message.chat.id,message.text)
+    bot.send_message(message.chat.id,"Stratton bot " + message.text)
 
 if __name__ == '__main__':
     bot.infinity_polling()
